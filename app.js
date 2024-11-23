dffd97915176e29d94889b81f0902a8b29dc3850
Nuradilovna <2303.02011@manas.edu.kg>
2024-11-23 15:42:59 +0600

    new commit

---
+++ app.js
@@ -0,0 +1,65 @@
+let music1 = document.getElementById("music1")
+music1.addEventListener("mouseover",function(event){
+    let sound = document.getElementById("Sound1")
+    sound.play();
+},false);
+music1.addEventListener("mouseout",function(event){
+    let sound = document.getElementById("Sound1")
+    sound.pause();
+    sound.currentTime = 0;
+})
+
+let music2 = document.getElementById("music2")
+music2.addEventListener("mouseover",function(event){
+    let sound = document.getElementById("Sound2")
+    sound.play();
+},false);
+music2.addEventListener("mouseout",function(event){
+    let sound = document.getElementById("Sound2")
+    sound.pause();
+    sound.currentTime = 0;
+})
+
+let music3 = document.getElementById("music3")
+music3.addEventListener("mouseover",function(event){
+    let sound = document.getElementById("Sound3")
+    sound.play();
+},false);
+music3.addEventListener("mouseout",function(event){
+    let sound = document.getElementById("Sound3")
+    sound.pause();
+    sound.currentTime = 0;
+})
+
+let music4 = document.getElementById("music4")
+music4.addEventListener("mouseover",function(event){
+    let sound = document.getElementById("Sound4")
+    sound.play();
+},false);
+music4.addEventListener("mouseout",function(event){
+    let sound = document.getElementById("Sound4")
+    sound.pause();
+    sound.currentTime = 0;
+})
+
+let music5 = document.getElementById("music5")
+music5.addEventListener("mouseover",function(event){
+    let sound = document.getElementById("Sound5")
+    sound.play();
+},false);
+music5.addEventListener("mouseout",function(event){
+    let sound = document.getElementById("Sound5")
+    sound.pause();
+    sound.currentTime = 0;
+})
+
+let music6 = document.getElementById("music6")
+music6.addEventListener("mouseover",function(event){
+    let sound = document.getElementById("Sound6")
+    sound.play();
+},false);
+music6.addEventListener("mouseout",function(event){
+    let sound = document.getElementById("Sound6")
+    sound.pause();
+    sound.currentTime = 0;
+})
